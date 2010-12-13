f6bf9f423a1d3dc47d374d0172fcc5b8b0671403
Eric Oestrich <eric@oestrich.org>
2010-12-13 01:14:22 -0500

    gvim config

--- init.sh
+++ init.sh
@@ -1,8 +1,11 @@
 #!/bin/bash
 
+# vim
 ln -s ~/dotfiles/vim ~/.vim
 ln -s ~/dotfiles/vimrc ~/.vimrc
+ln -s ~/dotfiles/gvimrc ~/.gvimrc
 
+# tmux
 ln -s ~/dotfiles/tmux.conf ~/.tmux.conf
 
 git submodule init
