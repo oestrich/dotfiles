3bbe27b39ec9d81c8c22dfbf283ae0ef56b34223
Eric Oestrich <eric@oestrich.org>
2011-01-12 09:36:53 -0500

    Git config

--- init.sh
+++ init.sh
@@ -5,6 +5,9 @@ ln -s ~/dotfiles/vim ~/.vim
 ln -s ~/dotfiles/vimrc ~/.vimrc
 ln -s ~/dotfiles/gvimrc ~/.gvimrc
 
+# git
+ln -s ~/dotfiles/gitconfig ~/.gitconfig
+
 # tmux
 ln -s ~/dotfiles/tmux.conf ~/.tmux.conf
 
