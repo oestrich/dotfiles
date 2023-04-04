7f75ddbed00ab54c6ef34614e4c31390407af807
Eric Oestrich <eric@oestrich.org>
2023-04-04 16:46:35 -0400

    Switch to just lazy.nvim

--- nvim/init.lua
+++ nvim/init.lua
@@ -1,8 +1,216 @@
 -- bootstrap lazy.nvim, LazyVim and your plugins
-require("config.lazy")
+-- require("config.lazy")
+
+local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
+if not vim.loop.fs_stat(lazypath) then
+  vim.fn.system({"git", "clone", "--filter=blob:none", "https://github.com/folke/lazy.nvim.git", "--branch=stable"})
+end
+vim.opt.rtp:prepend(lazypath)
+
+require("lazy").setup({
+  { "dstein64/vim-startuptime" },
+  { "echasnovski/mini.comment", version = false },
+  { "echasnovski/mini.indentscope", version = false },
+  { "ggandor/leap.nvim" },
+  { "goolord/alpha-nvim", dependencies = { "nvim-tree/nvim-web-devicons" } },
+  { "kevinhwang91/nvim-ufo", dependencies = { "kevinhwang91/promise-async" } },
+  { "navarasu/onedark.nvim" },
+  { "nvim-telescope/telescope.nvim", tag = "0.1.1", dependencies = { "nvim-lua/plenary.nvim" } },
+  { "nvim-treesitter/nvim-treesitter",
+    lazy = true,
+    build = ":TSUpdate",
+    event = { "BufReadPost", "BufNewFile" },
+    opts = {
+      ensure_installed = "all",
+      ignore_install = { "haskell", "phpdoc" },
+      highlight = { enable = true },
+      indent = { enable = true },
+    },
+    config = function(_, opts)
+      require("nvim-treesitter.configs").setup(opts)
+    end,
+  },
+  { "RRethy/vim-illuminate" },
+  { "tpope/vim-projectionist" },
+  { "tpope/vim-repeat" },
+})
+
+--
+-- options
+--
+
+vim.g.mapleader = ","
+vim.g.maplocalleader = ","
+
+vim.o.fillchars = [[eob: ,fold: ,foldopen:,foldsep: ,foldclose:]]
+
+vim.o.foldcolumn = "1"
+vim.o.foldlevel = 99
+vim.o.foldlevelstart = 99
+vim.o.foldenable = true
+
+local opt = vim.opt
+
+opt.expandtab = true -- Use spaces instead of tabs
+opt.list = true -- Show some invisible characters (tabs...
+opt.number = true -- Print line number
+opt.scrolloff = 4 -- Lines of context
+opt.shiftwidth = 2 -- Size of an indent
+opt.smartindent = true -- Insert indents automatically
+opt.spelllang = { "en" }
+opt.tabstop = 2 -- Number of spaces tabs count for
+opt.termguicolors = true -- True color support
+opt.wrap = false -- Disable line wrap
+
+--
+-- keymaps
+--
+
+-- Move to window using the <ctrl> hjkl keys
+vim.keymap.set("n", "<C-h>", "<C-w>h", { desc = "Go to left window" })
+vim.keymap.set("n", "<C-j>", "<C-w>j", { desc = "Go to lower window" })
+vim.keymap.set("n", "<C-k>", "<C-w>k", { desc = "Go to upper window" })
+vim.keymap.set("n", "<C-l>", "<C-w>l", { desc = "Go to right window" })
+
+-- Clear search with <esc>
+vim.keymap.set({ "i", "n" }, "<esc>", "<cmd>noh<cr><esc>", { desc = "Escape and clear hlsearch" })
+
+-- better indenting
+vim.keymap.set("v", "<", "<gv")
+vim.keymap.set("v", ">", ">gv")
+
+-- quit
+vim.keymap.set("n", "<leader>qq", "<cmd>qa<cr>", { desc = "Quit all" })
+
+-- windows
+vim.keymap.set("n", "<leader>ww", "<C-W>p", { desc = "Other window" })
+vim.keymap.set("n", "<leader>wd", "<C-W>c", { desc = "Delete window" })
+vim.keymap.set("n", "<leader>-", "<C-W>s", { desc = "Split window below" })
+vim.keymap.set("n", "<leader>|", "<C-W>v", { desc = "Split window right" })
+vim.keymap.set("n", "<C-w>-", "<C-W>s", { desc = "Split window below" })
+vim.keymap.set("n", "<C-w>|", "<C-W>v", { desc = "Split window right" })
+
+
+--
+-- autocmd
+--
+-- This file is automatically loaded by plugins.init
+
+local function augroup(name)
+  return vim.api.nvim_create_augroup("lazyvim_" .. name, { clear = true })
+end
+
+-- Check if we need to reload the file when it changed
+vim.api.nvim_create_autocmd({ "FocusGained", "TermClose", "TermLeave" }, {
+  group = augroup("checktime"),
+  command = "checktime",
+})
+
+-- Highlight on yank
+vim.api.nvim_create_autocmd("TextYankPost", {
+  group = augroup("highlight_yank"),
+  callback = function()
+    vim.highlight.on_yank()
+  end,
+})
+
+-- resize splits if window got resized
+vim.api.nvim_create_autocmd({ "VimResized" }, {
+  group = augroup("resize_splits"),
+  callback = function()
+    vim.cmd("tabdo wincmd =")
+  end,
+})
+
+-- go to last loc when opening a buffer
+vim.api.nvim_create_autocmd("BufReadPost", {
+  group = augroup("last_loc"),
+  callback = function()
+    local mark = vim.api.nvim_buf_get_mark(0, '"')
+    local lcount = vim.api.nvim_buf_line_count(0)
+    if mark[1] > 0 and mark[1] <= lcount then
+      pcall(vim.api.nvim_win_set_cursor, 0, mark)
+    end
+  end,
+})
+
+-- close some filetypes with <q>
+vim.api.nvim_create_autocmd("FileType", {
+  group = augroup("close_with_q"),
+  pattern = {
+    "PlenaryTestPopup",
+    "help",
+    "lspinfo",
+    "man",
+    "notify",
+    "qf",
+    "spectre_panel",
+    "startuptime",
+    "tsplayground",
+  },
+  callback = function(event)
+    vim.bo[event.buf].buflisted = false
+    vim.keymap.set("n", "q", "<cmd>close<cr>", { buffer = event.buf, silent = true })
+  end,
+})
+
+-- wrap and check for spell in text filetypes
+vim.api.nvim_create_autocmd("FileType", {
+  group = augroup("wrap_spell"),
+  pattern = { "gitcommit", "markdown" },
+  callback = function()
+    vim.opt_local.wrap = true
+    vim.opt_local.spell = true
+  end,
+})
+
+-- Auto create dir when saving a file, in case some intermediate directory does not exist
+vim.api.nvim_create_autocmd({ "BufWritePre" }, {
+  group = augroup("auto_create_dir"),
+  callback = function(event)
+    local file = vim.loop.fs_realpath(event.match) or event.match
+    vim.fn.mkdir(vim.fn.fnamemodify(file, ":p:h"), "p")
+  end,
+})
+
+--
+-- plugin setup
+--
 
 require("ufo").setup({
   provider_selector = function(bufnr, filetype, buftype)
     return { "treesitter", "indent" }
   end,
 })
+
+require("onedark").setup({
+  style = "darker"
+})
+
+require("onedark").load()
+
+require("telescope").setup({
+  defaults = { file_ignore_patterns = { "node_modules", "_build", "deps", "tmp" } }
+})
+
+local builtin = require("telescope.builtin")
+vim.keymap.set("n", "<leader>ff", builtin.find_files, {})
+vim.keymap.set("n", "<leader>fg", builtin.git_files, {})
+
+require("leap").add_default_mappings(true)
+
+require("mini.comment").setup()
+
+require("mini.indentscope").setup({
+  symbol = "│",
+  options = { try_as_border = true },
+})
+
+vim.api.nvim_create_autocmd("FileType", {
+  pattern = { "help", "alpha", "dashboard", "neo-tree", "Trouble", "lazy", "mason" },
+  callback = function()
+    vim.b.miniindentscope_disable = true
+  end,
+})
+
+require("alpha").setup(require("alpha.themes.startify").config)
